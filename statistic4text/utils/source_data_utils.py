140387fdd2c390f8fdb23ced581d593a36ad07bd
romus <vkromus@gmail.com>
2013-08-17 01:17:39 +0400

    Added: source for read blocks a file

--- statistic4text/utils/source_data_utils.py
+++ statistic4text/utils/source_data_utils.py
@@ -21,6 +21,7 @@ class Source():
 		"""
 		return None
 
+	@abstractmethod
 	def closeSource(self, source):
 		"""
 		Закрыть источник
@@ -66,10 +67,103 @@ class FileSource(Source):
 		:raise:  если не задан источник
 		"""
 		if not source:
-			raise  Exception("Source not found")
+			raise Exception("Source not found")
 
 		while True:
 			line = source.readline()
 			if not line:
 				break
 			yield line
+
+
+class FileBlockSource(FileSource):
+	""" Источник для работы по блокам с текстовыми файлами """
+
+	def __init__(self, blockSize=1024, maxSizeLastString=300, listSeparators=None):
+		"""
+		Инициализация
+
+		:param blockSize:  размер блока для чтения за один раз
+		:param maxSizeLastString: по-умолчанию - 300
+		:param listSeparators:  список с разделителями. По-умолчанию (если None) - [" ", "\n"]
+		"""
+		self.__blockSize = blockSize
+		self.__lastString = None
+		self.__maxSizeLastString = maxSizeLastString
+		self.__sizeLastString = 0
+
+		if not listSeparators:
+			listSeparators = [" ", "\n"]
+		self.__listSeparators = listSeparators
+		self.__stopIteration = False
+
+	def read(self, source):
+		self.__stopIteration = False
+		self.__lastString = None
+		self.__sizeLastString = 0
+		while True:
+			blockString = source.read(self.__blockSize)
+			if not blockString and self.__lastString:
+				tempLastString = self.__lastString
+				self.__stopIteration = True
+				yield tempLastString
+			elif not blockString:
+				self.__stopIteration = True
+
+			if self.__stopIteration:
+				break
+
+			if self.__lastString:  # было непрочитанное слово из предыдущего блока
+				sepIndex = self.__getFirstSeparator(blockString)
+				if sepIndex != -1:  # если был найден разделитель
+					if self.__sizeLastString + sepIndex + 1 <= self.__maxSizeLastString:
+						blockString = self.__lastString + blockString
+					else:  # бывает файл в одну строку
+						blockString = blockString[sepIndex: len(blockString)]
+					self.__lastString = None
+					self.__sizeLastString = 0
+
+			yield self.__getBlockString(blockString)
+
+	def __getBlockString(self, blockString):
+		"""
+		Получить обработанную строку
+
+		:param blockString:  прочитанная из файла строка
+		:return:  обработанная строка
+		"""
+		if not blockString[-1] in self.__listSeparators:  # есть непрочитанное слово
+			sepIndex = self.__getFirstSeparator(blockString, True)
+			if sepIndex == -1:  # прочитанный блок и есть непрочитанное слово
+				self.__lastString = self.__lastString + blockString if self.__lastString else blockString
+				blockString = ""
+			else:  # вырезаем непрочитанное слово
+				if self.__lastString:
+					self.__lastString = self.__lastString + blockString[sepIndex: len(blockString)]
+				else:
+					self.__lastString = blockString[sepIndex: len(blockString)]
+				blockString = blockString[0: sepIndex]
+			self.__sizeLastString = len(self.__lastString)
+
+		return blockString
+
+	def __getFirstSeparator(self, string, reverse=False):
+		"""
+		Найти первое значение разделителя
+
+		:param string:  строка
+		:param reverse:  True - поиск с конца строки
+		:return:  индекс найденного разделителя
+		"""
+		findSepIndex = -1
+		if string:
+			for itemSep in self.__listSeparators:
+				tempSepIndex = string.rfind(itemSep) if reverse else string.find(itemSep)
+				if reverse:
+					findSepIndex = tempSepIndex if (tempSepIndex > findSepIndex) else findSepIndex
+				else:
+					if (tempSepIndex != -1 and findSepIndex == -1) or \
+							(tempSepIndex != -1 and findSepIndex != -1 and findSepIndex < tempSepIndex):
+						findSepIndex = tempSepIndex
+
+		return findSepIndex
