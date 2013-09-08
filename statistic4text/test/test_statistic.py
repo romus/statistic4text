46328afbbb03da2a0ab3c522efe20ea8ae909958
romus <vkromus@gmail.com>
2013-09-08 16:20:45 +0400

    __mongoIndex ->  __mongoStatistics

--- statistic4text/test/test_statistic.py
+++ statistic4text/test/test_statistic.py
@@ -30,7 +30,7 @@ class TestMongoStatistic(unittest.TestCase):
 		self.__fileSourceCustom = FileSourceCustom()
 		self.__fileBlockSource = FileBlockSource()
 		self.__calcMongo = CalcMongo()
-		self.__mongoIndex = MongoStatistic(self.__mongoUtils)
+		self.__mongoStatistics = MongoStatistic(self.__mongoUtils)
 		self.__dirPath = os.path.abspath(os.curdir)
 
 	def tearDown(self):
@@ -42,29 +42,29 @@ class TestMongoStatistic(unittest.TestCase):
 	def testMakeDocStatisticCustomUtf8(self):
 		filePath = os.path.join(self.__dirPath, "resources/test_mongo_index_utf8")
 		self.__fileSourceCustom.custom = filePath
-		self.__mongoIndex.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
-		self.__mongoIndex.makeTotalStatistic()
+		self.__mongoStatistics.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
+		self.__mongoStatistics.makeTotalStatistic()
 
 	def testMakeDocStatisticCustomWin1251(self):
 		filePath = os.path.join(self.__dirPath, "resources/test_mongo_index_win1251")
 		self.__fileSourceCustom.custom = filePath
-		self.__mongoIndex.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
-		self.__mongoIndex.makeTotalStatistic()
+		self.__mongoStatistics.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
+		self.__mongoStatistics.makeTotalStatistic()
 
 	def testMakeDocStatisticCustomWin866(self):
 		filePath = os.path.join(self.__dirPath, "resources/test_mongo_index_win866")
 		self.__fileSourceCustom.custom = filePath
-		self.__mongoIndex.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
-		self.__mongoIndex.makeTotalStatistic()
+		self.__mongoStatistics.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
+		self.__mongoStatistics.makeTotalStatistic()
 
 	def testMakeDocStatistic(self):
 		data = "Проверка проверка сохранения индекса. Check save statistic"
-		self.__mongoIndex.makeDocStatistic("test_source_name", 1234, data, self.__simpleNormal)
-		self.__mongoIndex.makeTotalStatistic()
+		self.__mongoStatistics.makeDocStatistic("test_source_name", 1234, data, self.__simpleNormal)
+		self.__mongoStatistics.makeTotalStatistic()
 
 	def testAddMoreStatistics(self):
 		filePath = os.path.join(self.__dirPath, "resources/test_mongo_index_utf8")
 		self.__fileSourceCustom.custom = filePath
-		self.__mongoIndex.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
-		self.__mongoIndex.makeTotalStatistic()
+		self.__mongoStatistics.makeDocStatisticCustom(self.__fileBlockSource, self.__fileSourceCustom, self.__simpleNormal)
+		self.__mongoStatistics.makeTotalStatistic()
 		self.__mongoUtils.addMoreStatistics(self.__calcMongo)
